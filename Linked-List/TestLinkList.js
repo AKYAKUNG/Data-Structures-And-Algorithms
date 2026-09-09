9588b1e594f2e8e1e638a81ad6f2777a0f173bda
AKYAKUNG <aphichatthatlom517@gmail.com>
2026-09-09 11:14:14 +0700

    09/09/2569

--- Linked-List/TestLinkList.js
+++ Linked-List/TestLinkList.js
@@ -1,57 +1,9 @@
 import LinkedList from './LinkList.js';
 
-console.log("===== 1. ตรวจ Class =====");
-console.log(LinkedList);
-console.log(typeof LinkedList);
-
 let myLinkedList = new LinkedList(1);
-
-console.log("===== 2. หลังสร้าง LinkedList =====");
-console.log(myLinkedList);
-console.log("head:", myLinkedList.head);
-console.log("tail:", myLinkedList.tail);
-console.log("length:", myLinkedList.length);
-console.log("push:", typeof myLinkedList.push);
-console.log("pop:", typeof myLinkedList.pop);
-console.log("unshift:", typeof myLinkedList.unshift);
-
 myLinkedList.push(2);
-
-console.log("===== 3. หลัง push(2) =====");
-console.log(myLinkedList);
-console.log("head:", myLinkedList.head);
-console.log("tail:", myLinkedList.tail);
-console.log("length:", myLinkedList.length);
-
 myLinkedList.pop();
-
-console.log("===== 4. หลัง pop() =====");
-console.log(myLinkedList);
-console.log("head:", myLinkedList.head);
-console.log("tail:", myLinkedList.tail);
-console.log("length:", myLinkedList.length);
-
 myLinkedList.unshift(0);
-
-console.log("===== 5. หลัง unshift(0) =====");
-console.log(myLinkedList);
-console.log("head:", myLinkedList.head);
-console.log("tail:", myLinkedList.tail);
-console.log("length:", myLinkedList.length);
-
-console.log("get(0):", myLinkedList.get(0));
-
 myLinkedList.set(0, 5);
-
-console.log("===== 6. หลัง set(0, 5) =====");
+myLinkedList.reverse();
 console.log(myLinkedList);
-
-console.log("===== 7. ตรวจ Methods =====");
-console.log("push:", typeof myLinkedList.push);
-console.log("pop:", typeof myLinkedList.pop);
-console.log("unshift:", typeof myLinkedList.unshift);
-console.log("shift:", typeof myLinkedList.shift);
-console.log("get:", typeof myLinkedList.get);
-console.log("set:", typeof myLinkedList.set);
-console.log("insert:", typeof myLinkedList.insert);
-console.log("remove:", typeof myLinkedList.remove);
